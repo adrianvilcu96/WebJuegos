b1ae6593f74eabe02a33bdd429a27da5f2733acb
Adrian Vilcu <adrianvilcu96@gmail.com>
2020-01-16 18:12:35 +0100

    Hooks

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,6 +1,6 @@
 export const TETROMINOS={
     0:{shape:[[0]],color:'0,0,0'},
-    I:{
+    I:{//funciona
         shape:[
             [0,'I',0,0],
             [0,'I',0,0],
@@ -9,7 +9,7 @@ export const TETROMINOS={
         ],
         color:'80,227,230',
     },
-    J:{
+    J:{//funciona
         shape:[
             [0,'J',0],
             [0,'J',0],
@@ -17,7 +17,7 @@ export const TETROMINOS={
         ],
         color:'36,95,223',
     },
-    L:{
+    L:{//funciona
         shape:[
             [0,'L',0],
             [0,'L',0],
@@ -25,14 +25,14 @@ export const TETROMINOS={
         ],
         color:'223,173,36',
     },
-    O:{
+    O:{//funciona
         shape:[
             ['O','O'],
             ['O','O']
         ],
         color:'223,217,36',
     },
-    S:{
+    S:{//funciona
         shape:[
             [0,'S','S'],
             ['S','S',0],
@@ -40,7 +40,7 @@ export const TETROMINOS={
         ],
         color:'48,211,56',
     },
-    T:{
+    T:{//funciona
         shape:[
             [0,0,0],
             ['T','T','T'],
@@ -48,7 +48,7 @@ export const TETROMINOS={
         ],
         color:'132,61,198',
     },
-    Z:{
+    Z:{//funciona
         shape:[
             ['Z','Z',0],
             [0,'Z','Z'],
