f75987613e4a493998948452cb3cf006fdcac1af
David Saez Rodriguez <david.saez.rodriguez7@gmail.com>
2020-01-30 18:28:19 +0100

    añadido boton de vuelta en memory

--- src/components/Memory/Header/Header.js
+++ src/components/Memory/Header/Header.js
@@ -4,14 +4,18 @@ export default class Header extends Component {
     render() {
         return (
             <header>
-                <div className="titulo">React-Memoria</div>
+                <div className="titulo">React-Memoria
                 <div>
-                    <button className="boton-reiniciar" onClick={this.props.resetearPartida}> 
-                        Reiniciar
-            </button>
+                        <button className="boton-reiniciar" onClick={this.props.resetearPartida}>
+                            Reiniciar Juego
+                     </button>
+                    </div>
                 </div>
                 <div className="titulo">
                     Intentos: {this.props.numeroDeIntentos}
+                </div>
+                <div className="titulo">
+                    <a href="/" style={{ color: 'white' }}>Otros juegos</a>
                 </div>
             </header>
         );
